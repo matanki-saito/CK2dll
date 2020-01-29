764d1b2b259518a910e555a623de75c4907ddc90
matanki.saito <matanki.saito@gmail.com>
2020-01-30 01:37:08 +0900

    イベントダイアログの花文字の実装を追加

---
+++ Plugin64/decorative_letter.cpp
@@ -0,0 +1,77 @@
+﻿#include "pch.h"
+#include "plugin_64.h"
+
+namespace DecorativeLetter {
+	extern "C" {
+		void decorativeLetterProc1();
+		void decorativeLetterProc2();
+		uintptr_t decorativeLetterProc1ReturnAddress;
+		uintptr_t decorativeLetterProc2CallAddress;
+		uintptr_t decorativeLetterProc2ReturnAddress;
+	}
+
+	DllError decorativeLetterProc1Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_3_0:
+			// mov     r9d, 1
+			BytePattern::temp_instance().find_pattern("41 B9 01 00 00 00 45 33 C0 48 8D 95 30 12 00 00");
+			if (BytePattern::temp_instance().has_size(1, "ダイアログ")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// call    sub_140D5FDF0
+				decorativeLetterProc1ReturnAddress = address + 0x17;
+
+				Injector::MakeJMP(address, decorativeLetterProc1, true);
+			}
+			else {
+				e.unmatch.decorativeLetterProc1Injector = true;
+			}
+			break;
+		default:
+			e.version.decorativeLetterProc1Injector = true;
+		}
+
+		return e;
+	}
+
+	// 大文字化の処理がinline化されたので、別procにある修正が必要になった
+	DllError decorativeLetterProc2Injector(RunOptions options) {
+		DllError e = {};
+
+		switch (options.version) {
+		case v3_3_0:
+			// lea     rsi, [rax+rdi]
+			BytePattern::temp_instance().find_pattern("48 8D 34 38 0F B6 04 38");
+			if (BytePattern::temp_instance().has_size(1, "ダイアログ")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				// call {sub_xxxxx}
+				decorativeLetterProc2CallAddress = Injector::GetBranchDestination(address + 0x14).as_int();
+
+				// jnz	short loc_xxxxx
+				decorativeLetterProc2ReturnAddress = address + 0x23;
+
+				Injector::MakeJMP(address, decorativeLetterProc2, true);
+			}
+			else {
+				e.unmatch.decorativeLetterProc2Injector = true;
+			}
+			break;
+		default:
+			e.version.decorativeLetterProc2Injector = true;
+		}
+
+		return e;
+	}
+
+	DllError Init(RunOptions options) {
+		DllError result = {};
+
+		result |= decorativeLetterProc1Injector(options);
+		result |= decorativeLetterProc2Injector(options);
+
+		return result;
+	}
+}
---
+++ Plugin64/decorative_letter_asm.asm
@@ -0,0 +1,95 @@
+EXTERN	decorativeLetterProc1ReturnAddress	:	QWORD
+EXTERN	decorativeLetterProc2CallAddress	:	QWORD
+EXTERN	decorativeLetterProc2ReturnAddress	:	QWORD
+
+
+ESCAPE_SEQ_1	=	10h
+ESCAPE_SEQ_2	=	11h
+ESCAPE_SEQ_3	=	12h
+ESCAPE_SEQ_4	=	13h
+LOW_SHIFT		=	0Fh
+HIGH_SHIFT		=	9h
+SHIFT_2			=	LOW_SHIFT
+SHIFT_3			=	900h
+SHIFT_4			=	8F1h
+NO_FONT			=	98Fh
+NOT_DEF			=	2026h
+
+.CODE
+decorativeLetterProc1 PROC
+	lea		rdx, [rbp + 19B0h - 780h];
+	lea		rcx, [rbp + 19B0h - 16D8h];
+
+	cmp		qword ptr[rcx + 10h], 10h;
+	jb		JMP_A;
+	mov		rcx, [rcx];
+
+JMP_A:
+	mov		cl, byte ptr [rcx];
+
+	cmp		cl, ESCAPE_SEQ_1;
+	jz		JMP_B;
+
+	cmp		cl, ESCAPE_SEQ_2;
+	jz		JMP_B;
+
+	cmp		cl, ESCAPE_SEQ_3;
+	jz		JMP_B;
+
+	cmp		cl, ESCAPE_SEQ_4;
+	jz		JMP_B;
+
+	jmp		JMP_C;
+
+JMP_B:
+	mov		r9d,3;
+	jmp		JMP_D;
+
+JMP_C:
+	mov		r9d,1;
+
+JMP_D:
+	xor		r8d, r8d;
+	lea		rcx, [rbp + 19B0h - 16D8h]; Œ³‚É–ß‚·
+
+	push	decorativeLetterProc1ReturnAddress;
+	ret;
+decorativeLetterProc1 ENDP
+
+;----------------------;
+
+decorativeLetterProc2 PROC
+	lea		rsi, [rax + rdi];
+	movzx	eax, byte ptr [rax + rdi];
+	cmp		al, 0FEh;
+	jz		JMP_B;
+	cmp		al, ESCAPE_SEQ_1;
+	jz		JMP_A;
+	cmp		al, ESCAPE_SEQ_2;
+	jz		JMP_A;
+	cmp		al, ESCAPE_SEQ_3;
+	jz		JMP_A;
+	cmp		al, ESCAPE_SEQ_4;
+	jz		JMP_A;
+
+	movsx	ecx, al;
+	call	decorativeLetterProc2CallAddress;
+	mov		byte ptr [rsi], al;
+	jmp		JMP_C;
+
+JMP_B:
+	mov		byte ptr [rsi], 0DEh;
+	jmp		JMP_C;
+
+JMP_A:
+	add		ebx, 2;
+
+JMP_C:
+	inc		ebx;
+	mov		eax, ebx;
+	cmp		byte ptr [rbx + rdi], 0;
+
+	push	decorativeLetterProc2ReturnAddress;
+	ret;
+decorativeLetterProc2 ENDP
+END
